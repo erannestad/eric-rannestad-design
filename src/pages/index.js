34e299906ccf749b0ee28dc6981a2489bbc45530
Eric Rannestad <ericrannestad@gmail.com>
2022-06-07 14:34:10 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => {
   return (
     <main>
     <Layout pageTitle="Eric Rannestad">
-      <h3>Design + Cartography</h3>
+      <h3>Design</h3>
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
